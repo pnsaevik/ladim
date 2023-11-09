fc65682d7bc72d0b81c851811de2909f0fc9f024
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2023-11-09 08:15:13 +0100

    Split into two example directories

--- tests/test_ladim.py
+++ tests/test_ladim.py
@@ -6,6 +6,7 @@ import xarray as xr
 import json
 import io
 import numpy as np
+import pytest
 
 
 class Test_ladim_script:
@@ -15,47 +16,14 @@ class Test_ladim_script:
         assert output.stderr.decode('latin1') == ""
         assert output.stdout.decode('latin1').startswith("usage: ladim")
 
-    def test_can_advect_particles_when_old_version(self):
+    @pytest.mark.parametrize("example_num", range(1, 3))
+    def test_run_examples(self, example_num):
         curdir = Path.cwd()
         outfile = Path('out.nc')
-        testpath = Path(__file__).parent / 'sample_data'
+        name = f"ex{example_num}"
+        testpath = Path(__file__).parent / 'sample_data' / name
 
         with open(testpath / 'ladim.yaml') as f:
-            conf_str = f.read()
-
-        with open(testpath / 'output.nc_txt', 'r', encoding='utf-8') as f:
-            expected = json.load(f)
-
-        dset_dict = None
-        try:
-            os.chdir(testpath)
-            ladim.main(io.StringIO(conf_str))
-            dset = xr.load_dataset(str(outfile))
-            dset_dict = json.loads(json.dumps(
-                obj=dset.to_dict(),
-                default=str,
-            ))
-            for v in ['X', 'Y']:
-                d = dset_dict['data_vars'][v]['data']
-                dset_dict['data_vars'][v]['data'] = np.round(d, 3).tolist()
-
-        finally:
-            try:
-                outfile.unlink()
-            except IOError:
-                pass
-            os.chdir(curdir)
-
-        del expected['attrs']['date']
-        del dset_dict['attrs']['date']
-        assert dset_dict == expected
-
-    def test_can_advect_particles_when_new_version(self):
-        curdir = Path.cwd()
-        outfile = Path('out.nc')
-        testpath = Path(__file__).parent / 'sample_data'
-
-        with open(testpath / 'ladim_v2.yaml') as f:
             conf_str = f.read()
 
         with open(testpath / 'output.nc_txt', 'r', encoding='utf-8') as f:
