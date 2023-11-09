a54da495d15385b152854f012e63954e43226e32
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2023-11-09 09:25:55 +0100

    Add function for generating sorted interval

--- ladim/release.py
+++ ladim/release.py
@@ -1,4 +1,5 @@
 from .model import Model, Module
+import numpy as np
 
 
 class Releaser(Module):
@@ -23,3 +24,22 @@ class TextFileReleaser(Releaser):
     @property
     def particle_variables(self):
         return self._releaser.particle_variables
+
+
+def sorted_interval(v, a, b):
+    """
+    Searches for an interval in a sorted array
+
+    Returns the start (inclusive) and stop (exclusive) indices of
+    elements in *v* that are greater than or equal to *a* and
+    less than *b*. In other words, returns *start* and *stop* such
+    that v[start:stop] == v[(v >= a) & (v < b)]
+
+    :param v: Sorted input array
+    :param a: Lower bound of array values (inclusive)
+    :param b: Upper bound of array values (exclusive)
+    :returns: A tuple (start, stop) defining the output interval
+    """
+    start = np.searchsorted(v, a, side='left')
+    stop = np.searchsorted(v, b, side='left')
+    return start, stop
