81170d88d7f49657905b812c39b80c4a66f95261
Sævik Pål Næverlid <paal.naeverlid.saevik@hi.no>
2025-01-29 12:39:33 +0100

    Create grid stub functions

--- ladim/grid.py
+++ ladim/grid.py
@@ -1,4 +1,5 @@
 from .model import Module
+import numpy as np
 
 
 class Grid(Module):
@@ -22,6 +23,108 @@ class Grid(Module):
 
     def xy2ll(self, x, y):
         raise NotImplementedError
+
+    # --- MODERN METHODS ---
+
+    def scale_factors(
+            self, x: np.ndarray, y: np.ndarray
+    ) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Metric scale factors.
+
+        The metric scale factor dx is defined such that if one moves
+        a small increment delta_x along the X axis, then the distance
+        (in meters) equals dx * delta_x. The scale factor dy is defined
+        similarly.
+
+        :param x: X positions
+        :param y: Y positions
+        :return: A tuple dx, dy of scale factors [in meters per horizontal
+            grid units]
+        """
+        pass
+
+    def from_bearing(
+            self, x: np.ndarray, y: np.ndarray, b: np.ndarray
+    ) -> np.ndarray:
+        """
+        Azimutal angles from compass bearings.
+
+        A compass bearing (in degrees) is defined such that 0 is north, 90
+        is east, 180 is south and 270 is west. An azimutal vector angle is
+        defined such that 0 is pointing along the X axis, 90 is pointing
+        along the Y axis, 180 is pointing opposite the X axis and 270 is
+        pointing opposite the Y axis.
+
+        This function computes a set of azimutal vector angles from a set
+        of compass bearings and horizontal positions.
+
+        :param x: X positions
+        :param y: Y positions
+        :param b: Compass bearings [degrees]
+        :return: Azimutal vector angles [degrees]
+        """
+
+    def to_bearing(
+            self, x: np.ndarray, y: np.ndarray, az: np.ndarray
+    ) -> np.ndarray:
+        """
+        Azimutal angles from compass bearings.
+
+        A compass bearing (in degrees) is defined such that 0 is north, 90
+        is east, 180 is south and 270 is west. An azimutal vector angle is
+        defined such that 0 is pointing along the X axis, 90 is pointing
+        along the Y axis, 180 is pointing opposite the X axis and 270 is
+        pointing opposite the Y axis.
+
+        This function computes a set of compass bearings from a set
+        of azimutal vector angles and horizontal positions.
+
+        :param x: X positions
+        :param y: Y positions
+        :param az: Azimutal vector angles [degrees]
+        :return: Compass bearings [degrees]
+        """
+
+    def from_depth(
+            self, x: np.ndarray, y: np.ndarray, z: np.ndarray
+    ) -> np.ndarray:
+        """
+        Vertical coordinates from depth and horizontal coordinates.
+
+        :param x: X positions
+        :param y: Y positions
+        :param z: Depth below surface [m, positive downwards]
+        :return: Vertical coordinates
+        """
+
+    def to_depth(
+            self, x: np.ndarray, y: np.ndarray, s: np.ndarray
+    ) -> np.ndarray:
+        """
+        Depth from horizontal and vertical coordinates.
+
+        :param x: X positions
+        :param y: Y positions
+        :param s: Vertical coordinates
+        :return: Depth below surface [m, positive downwards]
+        """
+
+    def from_epoch(self, p: np.ndarray) -> np.ndarray:
+        """
+        Time coordinates from posix time
+
+        :param p: Posix time [seconds since 1970-01-01]
+        :return: Time coordinates
+        """
+
+    def to_epoch(self, t: np.ndarray) -> np.ndarray:
+        """
+        Posix time from time coordinates
+
+        :param t: Time coordinates
+        :return: Posix time [seconds since 1970-01-01]
+        """
 
 
 class RomsGrid(Grid):
