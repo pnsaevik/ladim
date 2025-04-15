6acfbd05621a021a0bd714890f71fe79cedaf829
Pål Næverlid Sævik <a5606@hi.no>
2025-04-15 14:14:56 +0200

    No quiet grep

--- .github/scripts/changelog.sh
+++ .github/scripts/changelog.sh
@@ -10,7 +10,7 @@ first_version_line=$(grep '^__version__' ladim/__init__.py | head -n 1)
 version_value=$(echo "$first_version_line" | sed -n "s/^__version__ = '\(.*\)'/\1/p")
 
 # Step 3: Check if the contents of the variable exist within the text file
-grep -q "[$version_value]" CHANGELOG.md
+grep "[$version_value]" CHANGELOG.md
 if grep -q "[$version_value]" CHANGELOG.md; then
     echo "The entry '[$version_value]' exists in CHANGELOG.md"
 else
