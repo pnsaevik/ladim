770b072e5ca79603dda08269a54edc1f5bfb2a83
Pål Næverlid Sævik <a5606@hi.no>
2025-04-15 14:28:03 +0200

    Fix search string

--- .github/scripts/changelog.sh
+++ .github/scripts/changelog.sh
@@ -8,11 +8,12 @@ first_version_line=$(grep '^__version__' ladim/__init__.py | head -n 1)
 
 # Step 2: Extract the substring within quotes from the __version__ line
 version_value=$(echo "$first_version_line" | sed -n "s/^__version__ = '\(.*\)'/\1/p")
-echo "Search for version: [$version_value]"
+search_text="## [$version] -"
+echo "Search for changelog entry: $search_text"
 
 # Step 3: Check if the contents of the variable exist within the text file
-grep "[$version_value]" CHANGELOG.md
-if grep -q "[$version_value]" CHANGELOG.md; then
+grep -F "$search_text" CHANGELOG.md
+if grep -qF "$search_text" CHANGELOG.md; then
     echo "The entry '[$version_value]' exists in CHANGELOG.md"
 else
     echo "The entry '[$version_value]' does not exist in CHANGELOG.md"
