530bb433179174065f70124312a177052dfe805c
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2025-11-14 07:40:39 +0100

    Add warm start skip to solver module

--- ladim/solver.py
+++ ladim/solver.py
@@ -20,7 +20,12 @@ class Solver:
         return Solver(start, stop, step, seed)
 
     def run(self, model: "Model"):
-        self.time = self.start
+        # Skip to first release time if possible
+        first_release_time = model.release.first_release_time()
+        seconds_to_skip = max(0, first_release_time - self.start)
+        timesteps_to_skip = seconds_to_skip // self.step
+        self.time = self.start + self.step * timesteps_to_skip
+
         while self.time <= self.stop:
             model.release.update(model)
             model.forcing.update(model)
