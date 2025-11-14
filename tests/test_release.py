ff76c6b6d1ffdf503f6e08cdfb260a19fe761825
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2025-11-14 07:38:05 +0100

    Add warm start capabilities

--- tests/test_release.py
+++ tests/test_release.py
@@ -3,6 +3,194 @@ import numpy as np
 import pytest
 import io
 import typing
+import netCDF4 as nc
+import pandas as pd
+
+
+class Test_Releaser_create:
+    def test_accepts_empty_parameter_list(self):
+        # Create empty releaser
+        r = release.Releaser.create()
+
+        # Retrieve particles from large time span
+        df = r.new_particles(start=0, stop=int(1e10))
+
+        # Check that particle list is empty
+        assert df.shape[0] == 0
+
+
+    def test_singular_releases(self):
+        # Create releaser with irregular release times
+        table = {'release_time': [0, 2, 5, 7, 10]}
+        r = release.Releaser.create(table)
+
+        # Retrieve particles from various time spans and confirm correct count
+        
+        df = r.new_particles(start=-10, stop=-1)  # Before start
+        assert df.shape[0] == 0     # No particles
+
+        df = r.new_particles(start=0, stop=1)     # Particle at start of interval
+        assert df.shape[0] == 1     # One particle
+
+        df = r.new_particles(start=0, stop=6)     # Particles at start and middle
+        assert df.shape[0] == 3     # Include all
+
+        df = r.new_particles(start=0, stop=7)     # Particles at start, middle and end
+        assert df.shape[0] == 3     # Include only start and middle
+
+        df = r.new_particles(start=1, stop=6)     # Particles at middle of interval
+        assert df.shape[0] == 2     # Include both
+
+        df = r.new_particles(start=1, stop=7)     # Particles at middle and end
+        assert df.shape[0] == 2     # Include only middle
+
+        df = r.new_particles(start=6, stop=7)     # Particle at end
+        assert df.shape[0] == 0     # Include nothing
+        
+        df = r.new_particles(start=20, stop=30 )  # After interval
+        assert df.shape[0] == 0     # No particles
+        
+    def test_simple_continuous_release(self):
+        # Create releaser with continuous release times
+        table = {'release_time': [10], 'release_interval': [3]}
+        r = release.Releaser.create(table)
+
+        # No particles before first release time
+        df = r.new_particles(start=5, stop=7)
+        assert df.shape[0] == 0
+
+        # Particles at every release interval afterwards
+        df = r.new_particles(start=10, stop=15)  # Start 1, middle 1
+        assert df.shape[0] == 2
+        df = r.new_particles(start=10, stop=19)  # Start 1, middle 2, end 1
+        assert df.shape[0] == 3
+
+
+class Test_reset_range_start:
+    def test_can_set_new_start(self):
+        start = 3
+        step = 2
+        limits = np.array([0, 1, 2, 3, 4, 5])
+        new_start = release.reset_range_start(start, step, limits)
+        assert np.asarray(new_start).tolist() == [1, 1, 3, 3, 5, 5]
+
+
+class Test_add_start_stop_step_to_release_table:
+    def test_adds_columns_when_discrete_spec(self):
+        tab = pd.DataFrame({'release_time': [0, 10, 30]})
+        result = release.add_start_stop_step_to_release_table(tab)
+        
+        start = result['release_start'].values
+        stop = result['release_stop'].values
+        step = result['release_step'].values
+
+        assert start.tolist() == [0, 10, 30]
+        assert stop[0] == 10
+        assert stop[1] == 30
+        assert stop[2] > 1_000_000
+        assert np.all(step > 1_000_000)
+
+    def test_adds_columns_when_continuous_spec(self):
+        tab = pd.DataFrame({
+            'release_time': [0, 10, 30],
+            'release_interval': [7, 2, 3]
+            })
+        result = release.add_start_stop_step_to_release_table(tab)
+        
+        start = result['release_start'].values
+        stop = result['release_stop'].values
+        step = result['release_step'].values
+
+        assert start.tolist() == [0, 10, 30]
+        assert stop[0] == 10
+        assert stop[1] == 30
+        assert stop[2] > 1_000_000
+        assert step.tolist() == [7, 2, 3]
+
+    def test_can_convert_string_datetimes(self):
+        tab = pd.DataFrame({'release_time': ['1970-01-01', '1970-01-02']})
+        result = release.add_start_stop_step_to_release_table(tab)
+        
+        start = result['release_start'].values
+        assert start.tolist() == [0, 86400]
+
+
+class Test_truncate_schedule:
+    def test_can_truncate(self):
+        tab = pd.DataFrame({
+            'release_start': [0, 0, 15],
+            'release_stop': [10, 20, 25],
+            'release_step': [2, 3, 5],
+        })
+        result = release.truncate_schedule_period(tab, 5, 15)
+        assert result.to_dict(orient='list') == {
+            'release_start': [6, 6],
+            'release_stop': [10, 15],
+            'release_step': [2, 3],
+        }
+
+    def test_removes_rows_outside_interval(self):
+        tab = pd.DataFrame({
+            'release_start': [0, 10],
+            'release_stop': [5, 15],
+            'release_step': [1, 1],
+        })
+        result = release.truncate_schedule_period(tab, 5, 10)
+        assert result.to_dict(orient='records') == []
+
+    def test_do_nothing_to_rows_inside_interval(self):
+        data = {
+            'release_start': [0, 10],
+            'release_stop': [5, 15],
+            'release_step': [2, 3],
+        }
+        tab = pd.DataFrame(data)
+        result = release.truncate_schedule_period(tab, 0, 15)
+        assert result.to_dict(orient='list') == data
+
+    def test_shifts_start_on_overlap(self):
+        data = {
+            'release_start': [0, 2],
+            'release_stop': [5, 15],
+            'release_step': [2, 3],
+        }
+        tab = pd.DataFrame(data)
+        result = release.truncate_schedule_period(tab, 3, 15)
+        assert result.to_dict(orient='list') == {
+            'release_start': [4, 5],
+            'release_stop': [5, 15],
+            'release_step': [2, 3],
+        }
+
+    def test_truncates_stop_on_overlap(self):
+        data = {
+            'release_start': [0, 2],
+            'release_stop': [10, 15],
+            'release_step': [2, 3],
+        }
+        tab = pd.DataFrame(data)
+        result = release.truncate_schedule_period(tab, 0, 5)
+        assert result.to_dict(orient='list') == {
+            'release_start': [0, 2],
+            'release_stop': [5, 5],
+            'release_step': [2, 3],
+        }
+
+
+class Test_expand_schedule:
+    def test_expands_row_by_row(self):
+        data = {
+            'release_start': [0, 5],
+            'release_stop': [6, 7],
+            'release_step': [2, 1],
+            'myfield': [10, 20],
+        }
+        tab = pd.DataFrame(data)
+        result = release.expand_schedule_range(tab)
+        assert result.to_dict(orient='list') == {
+            'release_time': [0, 2, 4, 5, 6],
+            'myfield': [10, 10, 10, 20, 20],
+        }
 
 
 class Test_TextFileReleaser_update:
@@ -147,166 +335,61 @@ class Test_TextFileReleaser_update:
         assert list(mock_model.state['X']) == [61]
 
 
-class Test_resolve_schedule:
-    def test_correct_when_all_events_are_specified(self):
-        e = list(release.resolve_schedule(
-            times=[0, 1, 1, 6, 9],
-            interval=10,
-            start_time=0,
-            stop_time=10,
-        ))
-        assert e == [0, 1, 2, 3, 4]
+class Test_load_last_timestep:
+    @pytest.fixture
+    def testdata(self, dataset):
+        dataset.createDimension('time')
+        dataset.createDimension('particle')
+        dataset.createDimension('particle_instance')
 
-    def test_correct_when_intermediate_points(self):
-        e = release.resolve_schedule(
-            times=[0, 1, 1, 1, 6, 6, 9],
-            interval=2,
-            start_time=0,
-            stop_time=10,
-        ).tolist()
-        assert e == [0] + [1, 2, 3] * 3 + [4, 5] * 2 + [6]
+        dataset.createVariable('particle_count', 'i4', 'time')
+        dataset.createVariable('pid', 'i4', 'particle_instance')
 
-    def test_correct_when_before_schedule(self):
-        # Repetition should start only after first time
-        e = release.resolve_schedule(
-            times=[0, 0, 3, 3, 3],
-            interval=2,
-            start_time=-4,
-            stop_time=6,
-        ).tolist()
-        assert e == [0, 1] * 2 + [2, 3, 4] * 2
+        dataset['particle_count'][:] = [1, 2, 3]
+        dataset['pid'][:] = [1, 1, 2, 1, 2, 4]
 
-    def test_correct_when_no_interval(self):
-        e = release.resolve_schedule(
-            times=[0, 0, 1, 1, 1, 2, 3, 4, 5],
-            interval=0,
-            start_time=1,
-            stop_time=4,
-        ).tolist()
-        assert e == [2, 3, 4, 5, 6]
+        yield dataset
 
-        e = release.resolve_schedule(
-            times=[0, 0, 1, 1, 1, 2, 3, 4, 5],
-            interval=0,
-            start_time=0,
-            stop_time=10,
-        ).tolist()
-        assert e == [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    def test_can_load_instance_variables(self, testdata):
+        testdata.createVariable('myinstvar', 'i4', 'particle_instance')
+        testdata['myinstvar'][:] = [11, 12, 22, 13, 23, 43]
+        df = release.load_last_timestep(testdata)
+
+        assert df['myinstvar'].values.tolist() == [13, 23, 43]
+
+    def test_can_load_time_variables(self, testdata):
+        testdata.createVariable('mytimevar', 'i4', 'time')
+        testdata['mytimevar'][:] = [10, 20, 30]
+        df = release.load_last_timestep(testdata)
+
+        assert df['mytimevar'].values.tolist() == [30, 30, 30]
+
+    def test_can_load_particle_variables(self, testdata):
+        testdata.createVariable('mypartvar', 'i4', 'particle')
+        testdata['mypartvar'][:] = [0, 10, 20, 30, 40]
+        df = release.load_last_timestep(testdata)
+
+        assert df['mypartvar'].values.tolist() == [10, 20, 40]
+
+    def test_converts_time_to_posix_timestamp(self, testdata):
+        testdata.createVariable('time', 'i4', 'time')
+        testdata.variables['time'].units = 'seconds since 1970-01-01'
+        testdata['time'][:] = [0, 3600, 7200]
+        df = release.load_last_timestep(testdata)
+
+        time = df['time'].to_numpy()
+        assert np.issubdtype(time.dtype, np.datetime64)
+        assert time.astype('datetime64[h]').astype(str).tolist() == [
+            '1970-01-01T02', '1970-01-01T02', '1970-01-01T02'
+        ]
 
 
-class Test_Schedule:
-    def test_can_expand(self):
-        # Event 0 and 1 are repeated twice when interval = 2
-        s1 = release.Schedule(times=np.arange(3) * 3, events=np.arange(3))
-        s2 = s1.expand(interval=2, stop=8)
-        assert s2.times.tolist() == [0, 2, 3, 5, 6]
-        assert s2.events.tolist() == [0, 0, 1, 1, 2]
 
-        # No elements in array; no expansion is possible
-        s1 = release.Schedule(times=np.array([]), events=np.array([]))
-        s2 = s1.expand(interval=2, stop=8)
-        assert s2.times.tolist() == []
-        assert s2.events.tolist() == []
-
-    def test_is_invalid_if_nondecreasing(self):
-        s = release.Schedule(times=np.array([3, 2, 1]), events=np.arange(3))
-        assert not s.valid()
-        s2 = release.Schedule(times=np.zeros(3), events=np.zeros(3))
-        assert s2.valid()
-
-    def test_can_append(self):
-        s1 = release.Schedule(np.arange(3), np.arange(3) * 2)
-        s2 = release.Schedule(np.arange(4), np.arange(4) * 2)
-        s3 = s1.append(s2)
-        assert s3.times.tolist() == [0, 1, 2, 0, 1, 2, 3]
-        assert s3.events.tolist() == [0, 2, 4, 0, 2, 4, 6]
-
-    def test_can_extend_backwards(self):
-        s1 = release.Schedule(np.ones(3), np.arange(3))
-        s2 = s1.extend_backwards(0)
-        assert s2.times.tolist() == [0, 0, 0, 1, 1, 1]
-        assert s2.events.tolist() == [0, 1, 2, 0, 1, 2]
-
-    def test_can_extend_backwards_using_interval(self):
-        s1 = release.Schedule(np.ones(3) * 4, np.arange(3))
-        s2 = s1.extend_backwards_using_interval(
-            time=1,
-            interval=2,
-        )
-        assert s2.times.tolist() == [0, 0, 0, 4, 4, 4]
-        assert s2.events.tolist() == [0, 1, 2, 0, 1, 2]
-
-    def test_can_trim_tail(self):
-        s1 = release.Schedule(np.arange(5), np.arange(5))
-
-        assert s1.trim_tail(4.0).times.tolist() == [0, 1, 2, 3]
-        assert s1.trim_tail(3.0).times.tolist() == [0, 1, 2]
-        assert s1.trim_tail(2.5).times.tolist() == [0, 1, 2]
-
-    def test_can_rightshift_time_values(self):
-        s1 = release.Schedule(np.array([0, 10, 10, 20, 30]), np.arange(5))
-
-        # Largest time value smaller than or equal to 17 is 10
-        # We add N=4 intervals to this time so that it becomes >= 17
-        s2 = s1.rightshift_closest_time_value(time=17, interval=2)
-        assert s2.times.tolist() == [0, 18, 18, 20, 30]
-
-        # Largest time value smaller than or equal to 17 is 10
-        # We could add N=1 intervals to this time so that it becomes >= 17
-        # ..but then the new value would become 25, which is larger than the
-        # next value, so this is not done. Instead, no elements are changed.
-        s2 = s1.rightshift_closest_time_value(time=17, interval=15)
-        assert s2.times.tolist() == [0, 10, 10, 20, 30]
-
-        # Largest time value smaller than or equal to 10 is 10
-        # This element does not have to change, it is already <= 10
-        s2 = s1.rightshift_closest_time_value(time=10, interval=2)
-        assert s2.times.tolist() == [0, 10, 10, 20, 30]
-
-        # No time value is smaller than -5
-        # No elements are changed
-        s2 = s1.rightshift_closest_time_value(time=-5, interval=2)
-        assert s2.times.tolist() == [0, 10, 10, 20, 30]
-
-        # Interval = 0 means nothing should change
-        s2 = s1.rightshift_closest_time_value(time=17, interval=0)
-        assert s2.times.tolist() == [0, 10, 10, 20, 30]
-
-        # No elements in schedule; nothing should change
-        s3 = release.Schedule(times=np.array([]), events=np.array([]))
-        s2 = s3.rightshift_closest_time_value(time=17, interval=2)
-        assert s2.times.tolist() == []
-
-        # One element in schedule; should be shifted
-        s3 = release.Schedule(times=np.array([10]), events=np.array([0]))
-        s2 = s3.rightshift_closest_time_value(time=17, interval=2)
-        assert s2.times.tolist() == [18]
-
-    def test_can_trim_head(self):
-        s = release.Schedule(
-            times=np.array([10, 10, 20, 20, 20, 30, 40]),
-            events=np.array([0, 1, 2, 3, 4, 5, 6]),
-        )
-
-        # Start time equals first time; no trimming
-        s2 = s.trim_head(start_time=10)
-        assert s2.times.tolist() == [10, 10, 20, 20, 20, 30, 40]
-        assert s2.events.tolist() == [0, 1, 2, 3, 4, 5, 6]
-
-        # Start time before first time; no trimming
-        s2 = s.trim_head(start_time=5)
-        assert s2.times.tolist() == [10, 10, 20, 20, 20, 30, 40]
-        assert s2.events.tolist() == [0, 1, 2, 3, 4, 5, 6]
-
-        # Start time after first time; remove first entries
-        s2 = s.trim_head(start_time=30)
-        assert s2.times.tolist() == [30, 40]
-        assert s2.events.tolist() == [5, 6]
-
-        # Start time after last time; remove all entries
-        s2 = s.trim_head(start_time=50)
-        assert s2.times.tolist() == []
-        assert s2.events.tolist() == []
+@pytest.fixture
+def dataset():
+    import uuid
+    with nc.Dataset(uuid.uuid4().hex, mode='a', diskless=True) as dset:
+        yield dset
 
 
 class MockObj:
