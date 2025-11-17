8977325b42010499dcd70fe60e3d744ce7e8ce40
a23236 <oeyvind.jakobsson@hi.no>
2025-11-17 14:28:12 +0100

    Moved the skip flag to the numerics part of the yaml file.

--- ladim/config.py
+++ ladim/config.py
@@ -83,7 +83,7 @@ def convert_1_to_2(c):
     out['solver']['step'] = dt_sec
     out['solver']['seed'] = dict_get(c, 'numerics.seed')
     out['solver']['skip_forwards_to_next_release_on_empty_state'] = \
-        dict_get(c,"skip_forwards_to_next_release_on_empty_state", False)
+        dict_get(c, "numerics.skip_forwards_to_next_release_on_empty_state", False)
 
     out['grid'] = {}
     out['grid']['file'] = dict_get(c, [
