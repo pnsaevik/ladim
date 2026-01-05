8844067bb16e6ea71a6db8317f8917f0034c6d56
OyvindJakobsson <oeyvind.jakobsson@hi.no>
2026-01-05 09:19:23 +0100

    Skip to next release on empty state. (#35)
    * Added remove_all_particles to state.py.
    
    * Added force_cache_update to ROMS forcing. This tool ignores the cached U, V , Unew, Vnew and all similar cached ibm forcing variables and resets them.
    
    * Added force_cache_update to Forcing module. This tool ignores the cached U, V , Unew, Vnew and all similar cached ibm forcing variables and resets them.
    
    * Added skip_forward tool to solver where we can skip over and start at a future point in time. This tool resets some cached variables and cleans out the list of particles.
    
    * Found and fixed a bug in the forcing update function where other forcing modules do not support forced cache updates. The program now exits if there is an attempt at skipping forward with a forcing module not supporting forced cache update.
    
    * Changed the forced update such that we now hold the temporal forcing delimiters of the last forcing step (self.prev_step and self.next_step) If a new t is outside these delimiters (inclusive), we force a cache update. Otherwise, we continue as before.
    
    * updated the controller script for skipping forward.
    
    * Set fixed python version
    
    * Update changelog
    
    * Ignore futurewarning in pytest
    
    * Fix issues with output module
    
    * Add warm start capabilities
    
    * Add warm start skip to solver module
    
    * Change update logic
    
    * added skip_forwards_to_next_release_on_empty_state functionality.
    
    Set the appropriate flag in the yaml file, and the solver loop will skip to the next release when it detects an empty state.
    
    * update pytest for new functionality
    
    * update pytest for new functionality
    
    * Moved the skip flag to the numerics part of the yaml file.
    
    * removed some debug code
    
    * removed some debug code
    
    * merged develop into update_t
    
    * Change criteria for forcing update
    
    * Fix netcdf install issue
    
    * Bump version number
    
    ---------
    
    Co-authored-by: Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>

--- tests/test_config.py
+++ tests/test_config.py
@@ -66,7 +66,8 @@ class Test_convert_1_to_2:
                         'ncformat': 'f4',
                         'positive': 'down',
                         'standard_name': 'depth_below_surface',
-                        'units': 'm'},
+                        'units': 'm',
+                    },
                     'pid': {
                         'long_name': 'particle identifier', 'ncformat': 'i4'},
                     'release_time': {
@@ -74,19 +75,24 @@ class Test_convert_1_to_2:
                         'long_name': 'particle release time',
                         'ncformat': 'i4',
                         'units': 'seconds since '
-                        '1970-01-01'}}},
+                        '1970-01-01',
+                    },
+                },
+            },
             'release': {
                 'colnames': ['release_time', 'X', 'Y', 'Z', 'group_id'],
                 'defaults': {},
                 'file': 'particles.rls',
                 'formats': {'time': 'release_time'},
                 'frequency': [1, 'm'],
-                'warm_start_file': None},
+                'warm_start_file': None,
+            },
             'solver': {
                 'seed': 0,
                 'start': datetime.datetime(2015, 9, 7, 1, 0),
                 'step': 60,
-                'stop': datetime.datetime(2015, 9, 7, 1, 5)},
+                'stop': datetime.datetime(2015, 9, 7, 1, 5),
+            },
             'tracker': {
                 'diffusion': 0.1,
                 'method': 'RK4',
